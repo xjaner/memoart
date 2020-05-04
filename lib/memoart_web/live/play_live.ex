e4b15ec4c262e942dd0812615a32a9a05f84aa3f
Xavier Janer <xavier.janer@qustodio.com>
2020-05-05 00:54:23 +0200

    Flip in a template

--- lib/memoart_web/live/play_live.ex
+++ lib/memoart_web/live/play_live.ex
@@ -1,94 +1,23 @@
 defmodule MemoartWeb.PlayLive do
   use MemoartWeb, :live_view
 
+  alias MemoartWeb.PlayView
+
   def mount(_params, _session, socket) do
-    socket = assign(socket, :a1flip, "front")
-    socket = assign(socket, :a2flip, "")
+    socket = assign(socket, :card_flip_1, "")
+    socket = assign(socket, :card_flip_2, "")
+    socket = assign(socket, :card_back_1, "kandinsky")
+    socket = assign(socket, :card_back_2, "gernika")
     {:ok, socket}
   end
 
   def render(assigns) do
-    ~L"""
-    <h3>Juga al Memoart!</h3>
-    <div class="container">
-    <table class="board">
-    <tr>
-    <td class="card">
-    <div class="flip-container">
-    <div class="flipper">
-    <div class="<%= @a1flip %>">
-    <a href="#" phx-click="a1"></a>
-    </div>
-    </div>
-    </div>
-
-    </td>
-    <td class="card">
-    <div class="flip-container <%= @a2flip %>">
-    <div class="flipper">
-    <div class="front">
-    <a href="#" phx-click="a2"></a>
-    </div>
-    <div class="back" id="back-a2">
-    <a href="#" phx-click="a2"></a>
-    </div>
-    </div>
-    </div>
-    </td>
-    <td class="card">
-
-    <div class="flip-container">
-    <div class="flipper">
-    <!--<div class="front">-->
-    <div class="<%= @a1flip %>">
-    <a href="#" phx-click="a1"></a>
-    </div>
-    <!--<div class="back" id="back-a1">
-    </div>-->
-    </div>
-    </div>
-
-    </td>
-    <td class="card">A4</td>
-    <td class="card">A5</td>
-    </tr>
-    <tr>
-    <td class="card">B1</td>
-    <td class="card">B2</td>
-    <td class="card">B3</td>
-    <td class="card">B4</td>
-    <td class="card">B5</td>
-    </tr>
-    <tr>
-    <td class="card">C1</td>
-    <td class="card">C2</td>
-    <td class="card">C3</td>
-    <td class="card">C4</td>
-    <td class="card">C5</td>
-    </tr>
-    <tr>
-    <td class="card">D1</td>
-    <td class="card">D2</td>
-    <td class="card">D3</td>
-    <td class="card">D4</td>
-    <td class="card">D5</td>
-    </tr>
-    <tr>
-    <td class="card">E1</td>
-    <td class="card">E2</td>
-    <td class="card">E3</td>
-    <td class="card">E4</td>
-    <td class="card">E5</td>
-    </tr>
-    </table>
-    </div>
-    """
+    PlayView.render("play.html", assigns)
   end
 
   def handle_event("a2", _, socket) do
     new_class = case socket.assigns.a2flip do
-      "" -> "g"
-      "g" -> "hover"
+      "" -> "hover"
       _ -> ""
     end
     IO.puts("A2 - a2flip:#{socket.assigns.a2flip} --> #{new_class}")
@@ -96,13 +25,14 @@ defmodule MemoartWeb.PlayLive do
     {:noreply, socket}
   end
 
-  def handle_event("a" <> card_id, _, socket) do
-    new_class = case socket.assigns.a1flip do
-      "front" -> "back"
-      _ -> "front"
+  def handle_event("card_click_" <> card_id, _, socket) do
+    flip_atom = String.to_atom("card_flip_#{card_id}")
+    new_class = case socket.assigns[flip_atom] do
+      "" -> "hover"
+      _ -> ""
     end
-    IO.puts("A#{card_id} - a1flip:#{socket.assigns.a1flip}")
-    socket = assign(socket, :a1flip, new_class)
+    IO.puts("A#{card_id}")
+    socket = assign(socket, flip_atom, new_class)
     {:noreply, socket}
   end
 end
